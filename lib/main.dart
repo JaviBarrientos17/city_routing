a64ea3e60e217f7db493714176de5d78cc230dae
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-09-28 16:03:54 +0200

    Added pkgs http & archive

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,54 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:archive/archive.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class HorariosPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _HorariosPageState createState() => _HorariosPageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _HorariosPageState extends State<HorariosPage> {
+  final String zipUrl =
+      'https://ssl.renfe.com/ftransit/Fichero_CER_FOMENTO/fomento_transit.zip';
 
-  final String title;
+  Future<void> fetchAndProcessData() async {
+    final response = await http.get(Uri.parse(zipUrl));
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+    if (response.statusCode == 200) {
+      final bytes = response.bodyBytes;
+      final archive = ZipDecoder().decodeBytes(bytes);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+      for (final file in archive) {
+        if (file.isFile) {
+          // Procesa el contenido de cada archivo dentro del ZIP
+          final fileContent = String.fromCharCodes(file.content);
+          print('Contenido del archivo ${file.name}: $fileContent');
+        }
+      }
+    } else {
+      print('Error en la solicitud: ${response.statusCode}');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('Horarios de trenes'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: fetchAndProcessData,
+          child: Text('Descargar y procesar horarios'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: HorariosPage(),
+  ));
 }
