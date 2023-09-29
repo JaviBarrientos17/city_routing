c7629922f53cca7c73c9ec0b4df4a1ed493bf366
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-09-29 11:52:44 +0200

    Centered title appBar

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Center(child: Text(widget.title, textAlign: TextAlign.center)),
       ),
       body: Center(
         child: Column(
