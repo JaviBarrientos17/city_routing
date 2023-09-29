d3f5dccd68fc34d784514ce00295e2d39431ed32
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-09-29 09:24:24 +0200

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
-import 'package:city_routing/classes/records.dart';
 import 'package:flutter/material.dart';
 import 'api/api.dart' as api;
+import 'package:city_routing/classes/records.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.deepPurple,
+        ),
       ),
       home: const MyHomePage(title: 'City Routing'),
     );
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title});
 
   final String title;
 
@@ -32,45 +33,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late Future<Records>? futureRecords; // Cambia el tipo a Future<Records>?
+  late Future<Records> _routeData;
 
   @override
   void initState() {
     super.initState();
-    futureRecords = api.fetchData();
+    _routeData = _fetchData(); // Asigna el resultado de la función _fetchData()
+  }
+
+  Future<Records> _fetchData() async {
+    try {
+      final response = await api.fetchData();
+      return response; // Retorna el resultado para asignarlo a _routeData
+    } catch (e) {
+      print('Error fetching data: $e');
+      throw e; // Relanza la excepción para manejarla en otro lugar si es necesario
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.primary,
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if (futureRecords == null)
-              const CircularProgressIndicator()
-            else
-              FutureBuilder<Records>(
-                future: futureRecords,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const CircularProgressIndicator();
-                  } else if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  } else if (snapshot.hasData) {
-                    final records = snapshot.data!;
-                    return Text('routeShortName: ${records.date}');
-                  } else {
-                    return const Text('No data available');
-                  }
-                },
-              )
-            // TODO - Revisar porque no renderiza bien los datos de la API. En consola los devuelve correctamente
-          ],
+        child: FutureBuilder<Records>(
+          future: _routeData,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (!snapshot.hasData) {
+              return const Text('No se recibieron datos de la API');
+            } else {
+              final records = snapshot.data!;
+
+              return ListView(
+                children: <Widget>[
+                  ListTile(
+                    title: Text('Date: ${records.date}'),
+                  ),
+                  ListTile(
+                    title: Text('Route Short Name: ${records.routeShortName}'),
+                  ),
+                  ListTile(
+                    title: Text('Trip Headsign: ${records.tripHeadsign}'),
+                  ),
+                  // Repite esto para los demás atributos de Records
+                ],
+              );
+            }
+          },
         ),
       ),
     );
