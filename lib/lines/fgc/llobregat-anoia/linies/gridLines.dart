23b971d86b59cb547b1f3add205d60d3a76e7fdb
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-10-01 14:06:46 +0200

    Resolve TODOs

--- lib/lines/fgc/llobregat-anoia/linies/gridLines.dart
+++ lib/lines/fgc/llobregat-anoia/linies/gridLines.dart
@@ -2,6 +2,8 @@
 
 import 'package:city_routing/lines/fgc/llobregat-anoia/linies/r5.dart';
 import 'package:city_routing/lines/fgc/llobregat-anoia/linies/r6.dart';
+import 'package:city_routing/lines/fgc/llobregat-anoia/linies/s3.dart';
+import 'package:city_routing/lines/fgc/llobregat-anoia/linies/s4.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,20 +15,29 @@ class GridLines extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        // TODO - Mostrar nombre de la línea a la cual se ha hecho clic en la pantalla anterior
         title: Text('Grid de Imágenes'),
       ),
       body: GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
         ),
-        itemCount: 2,
+        itemCount: 4,
         itemBuilder: (context, index) {
+          // TODO - Obtener las imgs del método routeImageManager()
           List<String> imageUrls = [
             'https://bit.ly/46tkIxZ',
-            'https://bit.ly/45fbTXk'
+            'https://bit.ly/45fbTXk',
+            'https://bit.ly/46wpJG4',
+            'https://bit.ly/45cQLkz'
           ];
 
-          List<Widget> destinationPages = [R5_Line(), R6_Line()];
+          List<Widget> destinationPages = [
+            R5_Line(),
+            R6_Line(),
+            S3_Line(),
+            S4_Line()
+          ];
 
           return GestureDetector(
             onTap: () {
